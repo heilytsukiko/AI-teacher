1b5d42219a2ac7383bb0067c66e15abfc4d194a4
Yerkemay <akasaigary@gmail.com>
2026-02-08 18:31:25 +0500

    feat: setup postgres with docker and auto-migrations

--- Backend/Migrations/20260208133103_InitialPostgres.Designer.cs
+++ Backend/Migrations/20260208133103_InitialPostgres.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20260208131259_InitialPostgres")]
+    [Migration("20260208133103_InitialPostgres")]
     partial class InitialPostgres
     {
         /// <inheritdoc />
